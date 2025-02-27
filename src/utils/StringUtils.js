04967434c127b2fc52ef56e08c3a6a5f44e4ef42
vishaluttirnaservice <vishaluttirnaservice@gmail.com>
2025-02-27 14:19:47 +0530

    feat:login data save cookie

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,4 +1,5 @@
 export const toPascalCase = (str) => {
+  if (!str) return null;
   return str
     .replace(/[_\s-]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ""))
     .replace(/^(.)/, (match) => match.toUpperCase());
