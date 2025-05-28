cd71ebdd32353d4aa464ce3821a172ef52589245
sachinuttirnaservice <sachinuttirnaservice@gmail.com>
2025-05-28 12:21:51 +0530

    feat : university search proeper implement

--- src/components/SearchComp/UniversitySearchBar.jsx
+++ src/components/SearchComp/UniversitySearchBar.jsx
@@ -1,104 +1,197 @@
+
+
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
-import { getUniversityCategory, getUniversityDist } from "./Api";
+import { getUniversityCategory, getUniversityDist, GetSearchUniversity } from "./Api";
+import { useEffect, useState } from "react";
+import { capitalize } from "../../utils/constansts";
 
-const UniversitySearchBar = () => {
-          const [universityCategoryValue, setUniversityCategoryValue] = useState("");
-          const [universityDistValue, setUniversityDistValue] = useState("");
+const UniversitySearchBar = ({
+  setQuery,
+  query,
+  setSearchUniversityData,
+  setIsLoading,
+}) => {
+  const [universityCategoryValue, setUniversityCategoryValue] = useState("");
+  const [universityDistValue, setUniversityDistValue] = useState("");
+  const [universitySearchParams, setUniversitySearchParams] = useState({
+    searchKey: "",
+    category: "",
+    type: "university",
+    dist: "",
+  });
 
-          const { data: ClassCategory } = useQuery({
-            queryKey: ["class-category"],
-            queryFn: getUniversityCategory,
-            refetchOnWindowFocus: false,
-          });
+  const { data: UniversityCategory } = useQuery({
+    queryKey: ["university-category"],
+    queryFn: getUniversityCategory,
+    refetchOnWindowFocus: false,
+  });
 
-          const { data: ClassDist } = useQuery({
-            queryKey: ["class-district"],
-            queryFn: getUniversityDist,
-            refetchOnWindowFocus: false,
-          });
+  const { data: UniversityDist } = useQuery({
+    queryKey: ["university-district"],
+    queryFn: getUniversityDist,
+    refetchOnWindowFocus: false,
+  });
 
-          const handleInputChange = () => {};
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["universities", universitySearchParams],
+    queryFn: () => GetSearchUniversity(universitySearchParams),
+    enabled: universitySearchParams?.type ? true : false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  console.log(universitySearchParams , 'query fin ')
+
+
+
+  useEffect(() => {
+    handleUniversitySearch();
+  }, []);
+
+  useEffect(() => {
+    if (data?.data) {
+      setSearchUniversityData(data.data);
+      setIsLoading(false);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    setIsLoading(isPending);
+  }, [isPending]);
+
+  useEffect(() => {
+    if (isError) {
+      setSearchUniversityData([]);
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handleUniversitySearch();
+    }, 1500);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [universitySearchParams, query]);
+
+  const handleUniversitySearch = () => {
+    setUniversitySearchParams((prev) => {
+      const newParams = {
+        searchKey: query || "",
+        category: universityCategoryValue,
+        type: "university",
+        dist: universityDistValue,
+      };
+      return newParams;
+    });
+  };
+
+  
+
+  const handleInputChange = (e) => {
+  const inputValue = e.target.value;
+  setQuery(inputValue);
+  setIsLoading(true);
+  
+  // Clear previous timeout
+  if (searchTimeout) clearTimeout(searchTimeout);
+  
+  // Set new timeout
+  setSearchTimeout(setTimeout(() => {
+    handleUniversitySearch();
+  }, 500));
+};
+
+useEffect(() => {
+  if (data) {
+    setSearchUniversityData(data); // <- Wrong structure
+    setIsLoading(false);
+  }
+}, [data]);
+
   return (
     <>
-      <div className="w-full max-w-5xl items-center mt-18 justify-center p-6 mx-auto">
-        {/* For desktop: Original horizontal layout */}
-        <div className="hidden md:flex w-full border-2 border-gray-400 bg-white rounded-full overflow-hidden sm:w-180 mx-auto">
-          <input
-            type="text"
-            className="px-4 py-3 w-full focus:outline-none"
-            placeholder={`Search Class`}
-            //   value={query}
-            onChange={handleInputChange}
-          />
-
-          <select
-            className="px-4 py-3 border-l border-gray-300 cursor-pointer"
-            onChange={(e) => setUniversityDistValue(e.target.value)}
-          >
-            <option value="">District</option>
-            {ClassDist?.data.map((district) => (
-              <option key={district} value={district}>
-                {district}
-              </option>
-            ))}
-          </select>
-
-          <select
-            className="px-4 py-3 border-l border-gray-300 cursor-pointer"
-            onChange={(e) => setUniversityCategoryValue(e.target.value)}
-          >
-            <option value="">Category</option>
-            {ClassCategory?.categories?.map((cate) => (
-              <option key={cate.category} value={cate.category}>
-                {cate.category}
-              </option>
-            ))}
-          </select>
-
-          <button className="rounded-r-full px-6 py-3 bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-            <IoSearchOutline className="text-2xl" />
-          </button>
-        </div>
-
-        {/* For mobile/tablet: Stacked layout */}
-        <div className="flex flex-col space-y-3 md:hidden w-full mx-auto">
-          <div className="w-full bg-white rounded-lg border-2 border-gray-400">
+      <div className="w-full sticky top-16 z-20 bg-white">
+        <div className="w-full max-w-6xl mx-auto px-4 py-3">
+          {/* Desktop Layout */}
+          <div className="hidden md:flex w-full bg-white border border-gray-200 rounded-full shadow-sm overflow-hidden">
             <input
               type="text"
-              className="px-4 py-3 w-full focus:outline-none rounded-lg"
-              placeholder={`Search University`}
-              // value={query}
+              className="px-6 py-3 w-full text-gray-700 placeholder-gray-400 focus:outline-none"
+              placeholder="Search University"
+              value={query}
               onChange={handleInputChange}
             />
-          </div>
 
-          <div className="w-full bg-white rounded-lg border-2 border-gray-400">
-            <select className="px-4 py-3 w-full focus:outline-none rounded-lg cursor-pointer">
+            <select
+              className="px-4 py-3 text-gray-700 border-l border-gray-200 bg-white cursor-pointer focus:outline-none"
+              onChange={(e) => setUniversityDistValue(e.target.value)}
+            >
               <option value="">District</option>
-              {ClassDist?.data.map((district) => (
+              {UniversityDist?.data.map((district) => (
                 <option key={district} value={district}>
-                  {district}
+                  {capitalize(district)}
                 </option>
               ))}
             </select>
-          </div>
 
-          <div className="w-full bg-white rounded-lg border-2 border-gray-400">
-            <select className="px-4 py-3 w-full focus:outline-none rounded-lg cursor-pointer">
+            <select
+              className="px-4 py-3 text-gray-700 border-l cursor-pointer border-gray-200 bg-white focus:outline-none"
+              onChange={(e) => setUniversityCategoryValue(e.target.value)}
+            >
               <option value="">Category</option>
-              {ClassCategory?.categories?.map((cate) => (
-                <option key={cate.category} value={cate.category}>
-                  {cate.category}
+              {UniversityCategory?.data?.map((cate) => (
+                <option key={cate} value={cate}>
+                  {capitalize(cate)}
                 </option>
               ))}
             </select>
+
+            <button className="bg-gradient-to-r from-purple-500 to-blue-500 px-6 py-3 text-white flex items-center justify-center hover:from-purple-600 hover:to-blue-600 transition-colors duration-200">
+              <IoSearchOutline className="text-2xl" />
+            </button>
           </div>
 
-          <button className="w-full rounded-lg px-6 py-3 bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-            <IoSearchOutline className="text-2xl mx-auto" />
-          </button>
+          {/* Mobile Layout */}
+          <div className="flex flex-col space-y-3 md:hidden w-full mt-4">
+            <input
+              type="text"
+              className="px-4 py-3 w-full border bg-white border-gray-300 rounded-lg focus:outline-none text-gray-700 placeholder-gray-400"
+              placeholder="Search University"
+              value={query}
+              onChange={handleInputChange}
+            />
+
+            <select
+              className="px-4 py-3 w-full border border-gray-300 rounded-lg text-gray-700 bg-white focus:outline-none"
+              onChange={(e) => setUniversityDistValue(e.target.value)}
+            >
+              <option value="">District</option>
+              {UniversityDist?.data.map((district) => (
+                <option key={district} value={district}>
+                  {capitalize(district)}
+                </option>
+              ))}
+            </select>
+
+            <select
+              className="px-4 py-3 w-full border border-gray-300 rounded-lg text-gray-700 bg-white focus:outline-none"
+              onChange={(e) => setUniversityCategoryValue(e.target.value)}
+            >
+              <option value="">Category</option>
+              {UniversityCategory?.data?.map((cate) => (
+                <option key={cate} value={cate}>
+                  {capitalize(cate)}
+                </option>
+              ))}
+            </select>
+
+            <button className="w-full px-6 py-3 rounded-lg bg-gradient-to-r from-purple-500 to-blue-500 text-white flex items-center justify-center hover:from-purple-600 hover:to-blue-600 transition-colors duration-200">
+              <IoSearchOutline className="text-2xl" />
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/components/UniversityComp/UniversityMultiCard.jsx
+++ src/components/UniversityComp/UniversityMultiCard.jsx
@@ -1,83 +1,68 @@
-import React, { useEffect, useState } from "react";
+
+
+
 import { useNavigate } from "react-router-dom";
 import UniversityCard from "./UniversityCard";
-import TagsSection from "../TagsSection";
-import { useSearchContext } from "../../store/SearchContext";
-import toast from "react-hot-toast";
-import { BounceLoader } from "react-spinners";
 import LoadingCard from "../loading-skeleton/LoadingCard";
 import UniversitySearchBar from "../SearchComp/UniversitySearchBar";
-import dataNotFound from '../../assets/images/dataNotFound.jpg'
-// import Loader from '../Loader';
+import dataNotFound from "../../assets/images/dataNotFound.jpg";
+import { useState } from "react";
 
 const UniversityMultiCard = () => {
   const navigate = useNavigate();
-
-  let { UniversityData, errorMsg, isLoading } = useSearchContext();
-
-  useEffect(() => {
-    toast.error(errorMsg || "Please try again later !");
-  }, [errorMsg]);
-
-  const tags = ["All", "Private", "Government"];
-
+  const [query, setQuery] = useState("");
+  const [searchUniversityData, setSearchUniversityData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
   return (
-    <div className="mt-10">
-      <UniversitySearchBar />
-      <TagsSection tags={tags} />
-      <div className="bg-gray-30 py-10">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-6 text-center">
-            Welcome to Education
-          </h2>
-          <p className="text-center text-gray-600 mb-10 max-w-xl mx-auto">
-            Discover the world's top universities with outstanding programs,
-            cutting-edge research, and vibrant campus communities.
-          </p>
-
-          {isLoading ? (
-            <div className="mt-10 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {[...Array(6)].map((_, index) => (
-                <LoadingCard key={index} />
-              ))}
-            </div>
-          ) : null}
-
-          {!isLoading && UniversityData?.results?.length !== 0 && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-              {UniversityData.results?.map((uni) => (
-                <UniversityCard
-                  key={uni._id}
-                  university={{
-                    id: uni._id,
-                    name: uni.universityName || "Unknown University",
-                    rank: uni.establishedYear || "N/A",
-                    category: uni.Category,
-                    location: `${uni.address?.line1 || "Address not available"}, ${uni.address?.state || "N/A"}`,
-                    image: uni.image || "https://via.placeholder.com/300",
-                    description:
-                      uni.info?.description || "No description available.",
-                  }}
-                  onClick={() => navigate(`/university/${uni._id}`)}
-                />
-              ))}
-            </div>
-          )}
-          {!isLoading && UniversityData?.results?.length !== 0 && (
-            <>
-              <div className="flex justify-center items-center flex-col mt-5">
-                <img
-                  src={dataNotFound}
-                  alt="No image found"
-                  className="w-40 sm:w-56 md:w-64 lg:w-72 xl:w-80 object-contain"
-                />
-                <h1 className="text-red-700">No University Data Found</h1>
-              </div>
-            </>
-          )}
-        </div>
+    <>
+      <UniversitySearchBar
+        setQuery={setQuery}
+        query={query}
+        setSearchUniversityData={setSearchUniversityData}
+        setIsLoading={setIsLoading}
+      />
+      
+      <div className="mt-18 px-4">
+        <h2 className="text-3xl font-bold text-center mb-6">
+          Explore Top Universities
+        </h2>
+        <p className="text-center text-gray-600 max-w-xl mx-auto">
+          Find the best universities with outstanding programs and excellent learning
+          opportunities.
+        </p>
       </div>
-    </div>
+
+        <div className="">
+        {isLoading ? (
+          <div className="mt-10 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {[...Array(6)].map((_, index) => (
+              <LoadingCard key={index} />
+            ))}
+          </div>
+        ) : searchUniversityData?.results?.length > 0 ? (
+          <div className="cursor-pointer grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 m-5">
+            {searchUniversityData.results.map((university, index) => (
+              <UniversityCard
+                university={university}
+                key={university.id || university._id || index}
+                onClick={() => navigate(`/university/${university._id}`)}
+              />
+            ))}
+            
+          </div>
+        ) : (
+          <div className="flex justify-center items-center flex-col mt-5">
+            <img
+              src={dataNotFound}
+              alt="No universities found"
+              className="w-40 sm:w-56 md:w-64 lg:w-72 xl:w-80 object-contain"
+            />
+            <h1 className="text-red-700">No University Data Found</h1>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
