048e2badc1428f1fefb8a2f3789a2746467e806a
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-07-21 10:20:36 +0530

    ref: file name updated

---
+++ src/components/Api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { BASE_URL } from '../utils/constansts';
+import { getAuthHeader } from '../utils/mics';
+
+
+
+export const getCollege = async (id) => {
+	const response = await axios.get(`${BASE_URL}/api/college/${id}`, {});
+	// console.log(response.data.data , 'get college ')
+	return response.data.data;
+};
+
+// 🔹 Logs user activity
+export const logUserActivityAPI = ({ userId, collegeId, token }) =>
+  axios.post(
+    `${BASE_URL}/api/user/activity`,
+    { userId, collegeId },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
